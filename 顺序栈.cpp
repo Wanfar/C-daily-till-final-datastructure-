95cf0fd25ebdd1a54fbd21ad5828c7ebbfa366df
Wanfar <47150528+Wanfar@users.noreply.github.com>
2019-12-05 00:26:10 +0800

    Add files via upload
    2019.12.5

---
+++ 顺序栈.cpp
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define maxsize 20
+typedef int selemtype;
+typedef struct{
+	selemtype *top;
+	selemtype *base;
+	int stacksize;
+}sqstack;
+
+int Initistack(sqstack &S){
+
+}
+
+int Gettop(sqstack S,selemtype &e){
+}
+
+int push(sqstack &S,selemtype e){
+}
+
+int pop(sqstack S,selemtype &e){
+}
+void main()
+{
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
